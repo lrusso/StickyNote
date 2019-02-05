01ffd4ebd995b89da8792d8e793737c023f33998
Leonardo Russo <contacto@lrusso.com.ar>
2019-02-04 22:49:35 -0300

    Add files via upload

--- StickyNoteUpdater.php
+++ StickyNoteUpdater.php
@@ -7,36 +7,51 @@ if ($_POST[length]!="")
 			{
 			if (strlen($_POST[mypostit])==$_POST[length])
 				{
+				// INSERT CODE FOR SAVING DATA HERE
 				// THE FINAL VALUE IS $_POST[mypostit]
+
+
+
+				// RETURNING THAT THE DATA WAS SAVED
+				header("HTTP/1.1 200 Internal Server");
+				header("Content-Type: text/plain; charset=UTF-8");
+				echo "1";die;exit;
 
 				}
 				else
 				{
-				header("HTTP/1.1 500 Internal Server");
-				header("Content-Type: application/json; charset=UTF-8");
-				die(json_encode(array("message" => "ERROR", "code" => 1)));
+				header("HTTP/1.1 200 Internal Server");
+				header("Content-Type: text/plain; charset=UTF-8");
+				echo "0";die;exit;
 				}
 			}
 			else
 			{
 			if (strlen($_POST[mypostit])==0 AND $_POST[length]=="0")
 				{
+				// INSERT CODE FOR SAVING DATA HERE
 				// THE FINAL VALUE IS EMPTY
 
+
+
+				// RETURNING THAT THE DATA WAS SAVED
+				header("HTTP/1.1 200 Internal Server");
+				header("Content-Type: text/plain; charset=UTF-8");
+				echo "1";die;exit;
 				}
 				else
 				{
-				header("HTTP/1.1 500 Internal Server");
-				header("Content-Type: application/json; charset=UTF-8");
-				die(json_encode(array("message" => "ERROR", "code" => 2)));
+				header("HTTP/1.1 200 Internal Server");
+				header("Content-Type: text/plain; charset=UTF-8");
+				echo "0";die;exit;
 				}
 			}
 		}
 		else
 		{
-		header("HTTP/1.1 500 Internal Server");
-		header("Content-Type: application/json; charset=UTF-8");
-		die(json_encode(array("message" => "ERROR", "code" => 3)));
+		header("HTTP/1.1 200 Internal Server");
+		header("Content-Type: text/plain; charset=UTF-8");
+		echo "0";die;exit;
 		}
 	}
 ?>
